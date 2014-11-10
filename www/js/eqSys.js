2ab128a984154b9477a43630513b1f6d29c6eca4
jhenaoz <jhenaoz93@gmail.com>
2014-11-10 09:00:36 -0500

    ahora si funcionando a lo bien

--- www/js/eqSys.js
+++ www/js/eqSys.js
@@ -385,7 +385,7 @@ function progresiveSustitution(Ab, n){
   var x= new Array(n);
   var result = "";
   x[0] = Ab[0][n] /Ab[0][0] ;
-  for(var i = 1; i <= n; i++){
+  for(var i = 1; i < n; i++){
       var sum = 0;
       for(var p = 0; p < i; p++){
           sum += Ab[i][p]*x[p];
